588f199d2bb3634a8f1ebf338d34426ee3902b8b
jo-ji-hyun <newterr@naver.com>
2025-08-25 15:28:58 +0900

    [Fix] popup창 키고 끄는 버튼 스크립트 추가

--- Assets/02.Scripts/Money/Bank.cs
+++ Assets/02.Scripts/Money/Bank.cs
@@ -1,14 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Bank : MonoBehaviour
 {
     public GameObject popupError;
 
+    public TextMeshProUGUI inputPlus;
+    public TextMeshProUGUI inputMinus;
+
+    private int _inputValue;
+
     private void Awake()
     {
         popupError.SetActive(false);
+    }
+
+    public void OnClickInputPlus()
+    {
+        string inputText = inputPlus.text;
+
+        if(int.TryParse(inputText, out _inputValue))
+        {
+            OnClickPlus(_inputValue);
+            Debug.Log("변형 성공");
+        }
+        else
+        {
+            Debug.LogError("숫자만 입력해주세요");
+        }
+
+
+    }
+
+    public void OnClickInputMinus()
+    {
+        string inputText = inputMinus.text;
+
+        _inputValue = int.Parse(inputText);
+
+        OnClickMinus(_inputValue);
     }
 
     public void OnClickPlus(int value)
---
+++ Assets/02.Scripts/UI/PopupBank.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupBank : MonoBehaviour
+{
+    public GameObject popupDeposit;
+    public GameObject popupWithDraw;
+
+    // === 입금시 팝업창 ===
+    public void OnClickDepositTrue()
+    {
+        popupDeposit.SetActive(true);
+    }
+
+    public void OnClickDepositFalse()
+    {
+        popupDeposit.SetActive(false);
+    }
+
+    // === 출금시 팝업창 ===
+    public void OnClickWithDrawTrue()
+    {
+        popupWithDraw.SetActive(true);
+    }
+
+    public void OnClickWithDrawFalse()
+    {
+        popupWithDraw.SetActive(false);
+    }
+}
